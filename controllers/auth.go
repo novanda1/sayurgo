2e0e4c4abf717c07d95414b78b54c538b6098bdd
Novanda Ahsan <novandaahsan1@gmail.com>
2021-12-10 14:34:23 +0700

    add auth dto

--- controllers/auth.go
+++ controllers/auth.go
@@ -2,11 +2,26 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/novanda1/sayurgo/models"
 	"github.com/novanda1/sayurgo/services"
 )
 
 func Auth(c *fiber.Ctx) error {
-	token, message, user := services.Auth(c)
+	body := new(models.User)
+	err := c.BodyParser(&body)
 
+	if err != nil {
+		c.JSON(fiber.Map{
+			"success": false,
+			"message": "failed to parse body",
+		})
+	}
+
+	errors := body.AuthDtoValidate(*body)
+	if errors != nil {
+		return c.JSON(errors)
+	}
+
+	token, message, user := services.Auth(body)
 	return c.JSON(fiber.Map{"token": token, "message": message, "user": user})
 }
--- models/user.go
+++ models/user.go
@@ -2,6 +2,9 @@ package models
 
 import (
 	"time"
+
+	"github.com/go-playground/validator/v10"
+	sharedTypes "github.com/novanda1/sayurgo/shared-types"
 )
 
 type UserAddress struct {
@@ -20,8 +23,25 @@ type UserAddress struct {
 type User struct {
 	ID          *string        `json:"id,omitempty" bson:"_id,omitempty"`
 	DisplayName *string        `json:"displayName,omitempty"`
-	Phone       *string        `json:"phone,omitempty"`
+	Phone       *string        `json:"phone,omitempty" validate:"required"`
 	UserAddress *[]UserAddress `json:"userAddress,omitempty"`
 	CreatedAt   time.Time      `json:"createdAt,omitempty"`
 	UpdatedAt   time.Time      `json:"updatedAt,omitempty"`
+}
+
+func (c User) AuthDtoValidate(user User) []*sharedTypes.ErrorResponse {
+	var errors []*sharedTypes.ErrorResponse
+	validate := validator.New()
+
+	err := validate.Struct(user)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			var element sharedTypes.ErrorResponse
+			element.FailedField = err.StructNamespace()
+			element.Tag = err.Tag()
+			element.Value = err.Param()
+			errors = append(errors, &element)
+		}
+	}
+	return errors
 }
