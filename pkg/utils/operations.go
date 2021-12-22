79345e113bc6bad3017b57bc364ea1e14e05572a
Novanda Ahsan <novandaahsan1@gmail.com>
2021-12-22 16:56:22 +0700

    appendOrSkip utils fix
    fixed -> panic: reflect: call of reflect.Value.IsNil on struct Value

--- pkg/utils/operations.go
+++ pkg/utils/operations.go
@@ -6,8 +6,19 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+func isNilFixed(i interface{}) bool {
+	if i == nil {
+		return true
+	}
+	switch reflect.TypeOf(i).Kind() {
+	case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
+		return reflect.ValueOf(i).IsNil()
+	}
+	return false
+}
+
 func AppendOrSkip(parent bson.D, name string, data interface{}) bson.D {
-	if data != nil && !reflect.ValueOf(data).IsNil() {
+	if data != nil && !isNilFixed(data) {
 		parent = append(parent, bson.E{Key: name, Value: data})
 		return parent
 	}
